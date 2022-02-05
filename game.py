d9f338cd0ea923ad4ae5889be5710321c1e83e9e
Petros <papaioannoupeter001@gmail.com>
2022-02-05 22:38:39 +0200

    Working check for vertical and horizontal rows

--- game.py
+++ game.py
@@ -1,35 +1,68 @@
-
 class tile:
     icon = str
-    id = 0
     def __init__(self, skin):
         self.icon = skin
+
+class board:
+    board = [str]*9
+    def __init__(self, init):
+        for i in range(0,9):
+            self.board[i] = init
+    def check_board(self):
+        check = 0
+        for i in range(0, 3):
+            if self.board[(3*i)] == self.board[(3*i)+1] and self.board[(3*i)+1] == self.board[(3*i)+2]:
+                if self.board[(3*i)] == "X":
+                    check = 1
+                    break
+                elif self.board[(3*i)] == "O":
+                    check = 2
+                    break
+                
+            if self.board[i] == self.board[i+3] and self.board[i+3] == self.board[i+6]:
+                if self.board[(3*i)] == "X":
+                    check = 1
+                    break
+                elif self.board[(3*i)] == "X":
+                    check = 2
+                    break
+        return check
+
+
 
 class Player:
     id = int
     moves = int
     tile_skin = str
-    board = [tile]*10
     def __init__(self, n, skin):
         self.id = n
         self.tile_skin = skin
-    def place(self, pos):
-        move = tile(self.tile_skin)#skin for tile
-        self.board[pos] = move#pos tile on board
+    def place(self, pos, board):
+        # move = tile(self.tile_skin)#skin for tile
+        board.board[pos] = self.tile_skin#pos tile on board
+        return board
 
 
 
 p1 = Player(1, "X")
 p2 = Player(2, "O")
 count = 0
-while True:
+myboard = board("_")
+winner = 0
+while winner == 0:
     p = input("GIVE POS")
     if int(p) == 0:
         break
     else:
-        if count % 2 == 0:
-            p1.place(int(p))
+        if count % 2 == 0: #Μπαίνει ανά 2 επαναλήψεις, άρα αποθηκεύει ανα δύο θέσεις αν τα δίνεις στη σειρά
+            myboard = p1.place(int(p)-1, myboard)
+        else:
+            myboard = p2.place(int(p)-1, myboard)
+    winner = myboard.check_board()
+    print("{}".format(winner))
     count += 1
 
-for j in range(0, 9):
-    print(p1.board[j].icon)
+
+print(myboard.board)
+# for j in range(0,3):
+#     print("{}{}{}".format(myboard.board[j+(3*0)],myboard.board[j+(3*0)+1],myboard.board[j+(3*0)+2]))
