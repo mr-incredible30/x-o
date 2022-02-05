f5dce645df2a901ab074df6f4c96427d9798640e
Petros <papaioannoupeter001@gmail.com>
2022-02-05 23:35:11 +0200

    Check for existing tiles

--- game.py
+++ game.py
@@ -50,19 +50,26 @@ count = 0
 myboard = board("_")
 winner = 0
 while winner == 0:
+    for i in range(0,9,3):
+        print(myboard.board[i+0],myboard.board[i+1],myboard.board[i+2])
     p = input("GIVE POS")
     if int(p) == 0:
         break
     else:
         if count % 2 == 0: #Μπαίνει ανά 2 επαναλήψεις, άρα αποθηκεύει ανα δύο θέσεις αν τα δίνεις στη σειρά
+            while myboard.board[int(p)-1] != "_":
+                p = input("GIVE pos")
             myboard = p1.place(int(p)-1, myboard)
         else:
+            while myboard.board[int(p)-1] != "_":
+                p = input("GIVE pos")
             myboard = p2.place(int(p)-1, myboard)
     winner = myboard.check_board()
-    print("{}".format(winner))
+    
     count += 1
-
-
-print(myboard.board)
+    
+for i in range(0,9,3):
+    print(myboard.board[i+0],myboard.board[i+1],myboard.board[i+2])
+print("Player {} WON".format(winner))
 # for j in range(0,3):
 #     print("{}{}{}".format(myboard.board[j+(3*0)],myboard.board[j+(3*0)+1],myboard.board[j+(3*0)+2]))
