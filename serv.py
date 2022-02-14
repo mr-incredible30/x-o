e30ecc22e87563578698d85eaa246305a4ed4139
Petros <papaioannoupeter001@gmail.com>
2022-02-14 20:43:46 +0200

    Car hacking 14/2

--- serv.py
+++ serv.py
@@ -1,38 +1,65 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect
 from random import randint
 
 app = Flask(__name__)
+games = {}
 
 class Game:
     def __init__(self):
-        self.player1_id = randint(100000, 999999)
+        self.game_id = str(randint(1000,9999))
+        self.player1_id = str(randint(100000, 999999))
         while True:
-            self.player2_id = randint(100000, 999999)
+            self.player2_id = str(randint(100000, 999999))
             if self.player1_id != self.player2_id:
                 break
         self.score = [0, 0]
         self.table = [['?', '?', '?'], ['?', '?', '?'], ['?', '?', '?']]
+        self.p1_played = False
+
+
     def pl_move(self, pl_id, pos_x, pos_y):
-        if self.player1_id == pl_id:
-            self.table[pos_x, pos_y] = "X"
+        if self.player1_id == pl_id and not self.p1_played:
+            self.table[pos_x][pos_y] = "X"
             status = 1
-        elif self.player2_id == pl_id:
-            self.table[pos_x, pos_y] = "O"
+            self.p1_played = True
+        elif self.player2_id == pl_id and self.p1_played:
+            self.table[pos_x][pos_y] = "O"
             status = 1
+            self.p1_played = False
         else:
             status = 0
         return status
-    def get_table(self):
-        return self.table
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/example_route')
-def example_route():
-    parameter = request.args.get("mykey")
-    return jsonify({"gay" : "nigger", "myparam" : parameter})
+@app.route('/new_game')
+def new_game():
+    game = Game()
+    games[game.game_id] = game
+    return jsonify({"game_id": game.game_id, "player_id": game.player1_id})
+
+@app.route('/join_game')
+def join_game():
+    game_id = request.args.get("game_id")
+    game = games[game_id]
+    return jsonify({"player_id": game.player2_id})
+
+@app.route('/get_game_board')
+def get_game_board():
+    game_id = request.args.get("game_id")
+    game = games[game_id]
+    return jsonify(game.table)
+
+@app.route('/move')
+def move():
+    game_id = request.args.get("game_id")
+    player_id = request.args.get("player_id")
+    pos_x = int(request.args.get("pos_x"))
+    pos_y = int(request.args.get("pos_y"))
+    games[game_id].pl_move(player_id, pos_x, pos_y)
+    return "OK"
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -3,90 +3,43 @@ let score = [0,0];
 let c = document.getElementById("nigger").children;
 let m = document.getElementById("win_mes");
 let s = document.getElementById("sc");
-let turn = 0;
+
+var game_id;
+var player_id;
+
+async function createGame() {
+    let response = await fetch('/new_game');
+    let data = await response.json();
+
+    game_id = data.game_id;
+    player_id = data.player_id;
+
+    document.getElementById("game_id").innerHTML = `Game ID: ${game_id}`;
+    document.getElementById("gameButtons").style.display = 'none';
+}
+
+async function joinGame() {
+    let id = document.getElementById("game_id_field").value;
+    let response = await fetch(`/join_game?game_id=${id}`);
+    if (response.ok) {
+        let data = await response.json();
+        game_id = id;
+        player_id = data.player_id;
+
+        document.getElementById("game_id").innerHTML = `Game ID: ${game_id}`;
+        document.getElementById("gameButtons").style.display = 'none';
+    }
+}
 
 function buttonPressed(row, col) {
     console.log(`Button on row ${row} and col ${col} pressed`);
-    setPlayed(row, col);
-    let win = check_win();
-    if (win == 1){
-        console.log(`Player 1 Won!`);
-        for (let i = 0; i < size; i++) {
-            for (let j = 0; j < size; j++) {
-                c[i].children[j].children[0].setAttribute("disabled", "");
-            }
-        }
-        let w = document.createElement("h1");
-        w.innerHTML = "Player 1 Won";
-        m.appendChild(w);
-        score[0]++;
-    }
-    else if (win == 2){
-        console.log(`Player 2 Won!`);
-        for (let i = 0; i < size; i++) {
-            for (let j = 0; j < size; j++) {
-                c[i].children[j].children[0].setAttribute("disabled", "");
-            }
-        }
-        let w = document.createElement("h1");
-        w.innerHTML = "Player 2 Won";
-        m.appendChild(w);
-        score[1]++;
-    }
+    fetch(`/move?game_id=${game_id}&player_id=${player_id}&pos_x=${row}&pos_y=${col}`);
 }
 
 function setPlayed(row, col) {
     c[row].children[col].children[0].setAttribute("disabled", "");
-    c[row].children[col].children[0].innerHTML = turn % 2 == 0 ? "X" : "O";
-    /*
-    if (turn % 2 == 0) vale X
-    else vale O
-    */
-    turn++;
 }
 
-function check_win(){
-    let win = 0;
-    for (let i = 0; i < size; i++) {
-        if (c[i].children[0].children[0].innerHTML == c[i].children[1].children[0].innerHTML && c[i].children[1].children[0].innerHTML == c[i].children[2].children[0].innerHTML){
-            if (c[i].children[0].children[0].innerHTML == "X"){
-                win = 1;
-                break;
-            }
-            else if (c[i].children[0].children[0].innerHTML == "O"){
-                win = 2;
-                break;
-            }
-        }
-        if (c[0].children[i].children[0].innerHTML == c[1].children[i].children[0].innerHTML && c[1].children[i].children[0].innerHTML == c[2].children[i].children[0].innerHTML){
-            if (c[0].children[i].children[0].innerHTML == "X"){
-                win = 1;
-                break;
-            }
-            else if (c[0].children[i].children[0].innerHTML == "O"){
-                win = 2;
-                break;
-            }
-        }               
-    }    
-    if (c[0].children[0].children[0].innerHTML == c[1].children[1].children[0].innerHTML && c[1].children[1].children[0].innerHTML == c[2].children[2].children[0].innerHTML){
-        if (c[0].children[0].children[0].innerHTML == "X"){
-            win = 1;
-        }
-        else if (c[0].children[0].children[0].innerHTML == "O"){
-            win = 2;
-        }
-    }
-    else if (c[0].children[2].children[0].innerHTML == c[1].children[1].children[0].innerHTML && c[1].children[1].children[0].innerHTML == c[2].children[0].children[0].innerHTML){
-        if (c[0].children[2].children[0].innerHTML == "X"){
-                win = 1;
-            }
-        else if (c[0].children[2].children[0].innerHTML == "O"){
-            win = 2;
-        }
-    }
-    return win;
-}
 function set_game(){
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
@@ -102,38 +55,19 @@ function set_game(){
     h.innerHTML = "Player 1| "+score[0]+" | "+score[1]+" |Player 2 ";
     sc.appendChild(h);
 }
-function del_game(){
+
+async function update_board(){
+    if (!game_id) return;
+    let response = await fetch(`/get_game_board?game_id=${game_id}`)
+    let board = await response.json();
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
-            c[i].children[j].children[0].remove();
+            c[i].children[j].children[0].innerHTML = board[i][j];
         }
-    }
-    if (m.children.length) m.children[0].remove();
-    sc.children[1].remove();
-}
-
-let re = document.getElementById('reset');
-re.onclick = function(){
-    del_game();
-    set_game();
+    }    
 }
 set_game();
 
-/*let request = fetch('/example_route');
-request.then(function(response) {
-    response.json().then(function(data) {
-        console.log(data);
-    });
-});*/
-
-async function gay() {
-    let parameter = "gaynigger"
-    let response = await fetch(`/example_route?mykey=${parameter}`);
-    let data = await response.json();
-    //console.log(data);
-    return data;
-}
-
-gay().then(function(data) {
-    console.log(data);
-});
+setInterval(function() {
+    update_board();
+}, 1000);
